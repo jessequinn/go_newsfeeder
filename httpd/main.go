97d8a60a9256cf6b9bb52ff0ebb714de18927697
Jesse Quinn <me@jessequinn.info>
2019-12-14 17:52:30 -0300

    initial commit with go

---
+++ httpd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"NewsFeeder/httpd/handler"
+	"NewsFeeder/platform/newsfeed"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	feed := newsfeed.New()
+	r := gin.Default()
+	r.GET("/ping", handler.PingGet())
+	r.GET("/newsfeed", handler.NewsfeedGet(feed))
+	r.POST("/newsfeed", handler.NewsfeedPost(feed))
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ platform/newsfeed/newsfeed_test.go
@@ -0,0 +1,22 @@
+package newsfeed
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	feed := New()
+	feed.Add(Item{"An item", "with body"})
+
+	if len(feed.Items) != 1 {
+		t.Errorf("Item was not added")
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	feed := New()
+	feed.Add(Item{"An item", "with body"})
+	results := feed.GetAll()
+
+	if len(results) != 1 {
+		t.Errorf("Item was not added")
+	}
+}
